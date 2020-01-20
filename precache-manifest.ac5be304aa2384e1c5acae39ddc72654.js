57883c942f6bd2f6ce52c7a43e8cdf0a6ef21320
Земцева Виктория <jlgfmhldf@gmail.com>
2020-01-20 09:41:37 +0300

    Updates

---
+++ precache-manifest.ac5be304aa2384e1c5acae39ddc72654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163fd029d4f352090aef135124ff846",
+    "url": "/cookies-games-test/index.html"
+  },
+  {
+    "revision": "e70068bca01c4a9259f8",
+    "url": "/cookies-games-test/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "75d31f3e9b4a8b336b01",
+    "url": "/cookies-games-test/static/css/main.15bc10af.chunk.css"
+  },
+  {
+    "revision": "e70068bca01c4a9259f8",
+    "url": "/cookies-games-test/static/js/2.f60640bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cookies-games-test/static/js/2.f60640bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75d31f3e9b4a8b336b01",
+    "url": "/cookies-games-test/static/js/main.344f086d.chunk.js"
+  },
+  {
+    "revision": "ff2fb25a238ea6502db1",
+    "url": "/cookies-games-test/static/js/runtime-main.4a399250.js"
+  }
+]);
